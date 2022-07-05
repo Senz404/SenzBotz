fec7073223bbef01fa527db68ebdb4967bfc3ad7
Senz404 <108679988+Senz404@users.noreply.github.com>
2022-07-05 13:28:38 +0700

    © Senz

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285822347348','6285822347348','6285822347348']
-global.premium = ['6285822347348']
+global.owner = ['6283800930156','6283800930156','6283800930156']
+global.premium = ['6283800930156']
 global.packname = 'Sticker by'
-global.author = 'ArullOfc'
+global.author = 'Senz'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
